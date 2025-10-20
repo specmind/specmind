a4a9ca2192567ca7cb8601fd7335cd5a290ce2ce
Mushegh Gevorgyan <gmusho.net@gmail.com>
2025-10-19 20:30:07 -0700

    chore: bump all packages to version 0.1.2
    - Update @specmind/core to 0.1.2
    - Update @specmind/format to 0.1.2
    - Update specmind CLI to 0.1.2
    - Update all internal dependencies to use 0.1.2
    - Add publishConfig to all packages for public npm access

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,7 +9,7 @@ const program = new Command()
 program
   .name('specmind')
   .description('AI-powered architecture documentation tool')
-  .version('0.1.0')
+  .version('0.1.2')
 
 // Analyze command
 program
